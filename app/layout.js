03b191dfb2348335169f64e52ec5c4743a4582a4
abdulfaiq47 <abdulfaiq47@gmail.com>
2025-09-26 09:50:10 +0500

    Initial commit of project

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "@/providers";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +23,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Providers>
+
+          {children}
+        </Providers>
+
       </body>
     </html>
   );
---
+++ app/login/page.js
@@ -0,0 +1,18 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import style from './page.module.css'
+import Image from "next/image";
+
+export default function login() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <p>Signed in as {session.user.email}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return <button className={style.googlebtn} onClick={() => signIn("google")}> <Image src="/googlelogo.svg" height={30} width={40} style={{ marginLeft: "-9px" }} alt="Google Logo" ></Image> Sign in with Google</button>;
+}
